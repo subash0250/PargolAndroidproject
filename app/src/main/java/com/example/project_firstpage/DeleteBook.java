b3c95a1466a2df07d05cce5d2be51ea578d076a2
Subash Gaddam <subashgaddam@Subashs-Laptop.local>
2024-07-22 19:30:21 -0400

    book delete page add

---
+++ app/src/main/java/com/example/project_firstpage/DeleteBook.java
@@ -0,0 +1,18 @@
+package com.example.project_firstpage;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DeleteBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_book);
+    }
+}
