b42ae3f3f35a3a878cd05330846ac65c63121186
Subash Gaddam <subashgaddam@Subashs-Laptop.local>
2024-07-22 19:44:05 -0400

    Work on book dlete confirm and cancel button

--- app/src/main/java/com/example/project_firstpage/DeleteBook.java
+++ app/src/main/java/com/example/project_firstpage/DeleteBook.java
@@ -1,6 +1,8 @@
 package com.example.project_firstpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DeleteBook extends AppCompatActivity {
-
+    Button confirm_delete_book_btn, cancel_delete_book_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_book);
+        confirm_delete_book_btn = findViewById(R.id.confirm_delete_book_btn);
+        cancel_delete_book_btn = findViewById(R.id.cancel_delete_book_btn);
+
+        confirm_delete_book_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // delete book
+            }
+        });
+
+        cancel_delete_book_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go back
+            }
+        });
+
+
     }
 }
