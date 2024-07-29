a04d909a015616f91be74e8ae62f6a702b52fa91
Subash Gaddam <subashgaddam@Subashs-Laptop.local>
2024-07-28 21:17:53 -0400

    User class create

--- app/src/main/java/com/example/project_firstpage/Accountcreation.java
+++ app/src/main/java/com/example/project_firstpage/Accountcreation.java
@@ -132,17 +132,5 @@ public class Accountcreation extends AppCompatActivity {
         });
     }
 
-    public static class User {
-        public String firstName;
-        public String lastName;
-        public String email;
-        public String role;
 
-        public User(String firstName, String lastName, String email, String role) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.role = role;
-        }
-    }
 }
