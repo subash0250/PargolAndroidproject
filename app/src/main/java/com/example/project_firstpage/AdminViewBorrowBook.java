3fb7a01f6ca69f72e1f83c51f3e84c65ae3d91e7
Subash Gaddam <subashgaddam@Subashs-Laptop.local>
2024-08-13 12:43:58 -0400

    adding borrow bokks by users to admin

--- app/src/main/java/com/example/project_firstpage/AdminViewBorrowBook.java
+++ app/src/main/java/com/example/project_firstpage/AdminViewBorrowBook.java
@@ -1,6 +1,9 @@
 package com.example.project_firstpage;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AdminViewBorrowBook extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
+public class AdminViewBorrowBook extends AppCompatActivity {
+    private TextView tvTitle, tvAuthor,tvlanguage,tvgener, tvavailability, tvBorrowDate;
+    private ImageView ivimage;
+    private Button btnBack, btnReturn;
+    private String bookId, title, author, language, gener, image, borrowDate ;
+    private Boolean isAvailable;
+    private DatabaseReference booksRef, books;
+    FirebaseAuth mAuth;
+    private  String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_view_borrow_book);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+    });
     }
 }
