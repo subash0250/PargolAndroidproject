491f56e2919ea4aa2404afc9f1bfa4b62d7e72a8
subash <gaddamsubashreddy@gmail.com>
2024-08-15 15:18:18 -0400

    created ids for xml in java file

--- app/src/main/java/com/example/project_firstpage/AdminViewBorrowBook.java
+++ app/src/main/java/com/example/project_firstpage/AdminViewBorrowBook.java
@@ -1,6 +1,8 @@
 package com.example.project_firstpage;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,13 +27,28 @@ public class AdminViewBorrowBook extends AppCompatActivity {
     FirebaseAuth mAuth;
     private  String userId;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_view_borrow_book);
         mAuth = FirebaseAuth.getInstance();
+        //        FirebaseUser user = mAuth.getCurrentUser();
+//        if(user != null){
+//            userId = user.getUid();
+//        }
+        userId = getIntent().getStringExtra("userId");
+        Log.e("userId", userId);
+        btnBack = findViewById(R.id.btnBack);
+        btnReturn = findViewById(R.id.btnReturn);
+        tvTitle = findViewById(R.id.Title);
+        tvAuthor = findViewById(R.id.Author);
+        tvlanguage = findViewById(R.id.Language);
+        tvgener = findViewById(R.id.Genere);
+        ivimage = findViewById(R.id.Image);
+        //tvavailability = findViewById(R.id.Availability);
+        tvBorrowDate = findViewById(R.id.BorrowDate);
 
     }
 }
