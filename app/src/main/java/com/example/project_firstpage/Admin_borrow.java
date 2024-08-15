1bf7acd3e8e1b913de01434e5a5da5a7ce4a2e02
subash <gaddamsubashreddy@gmail.com>
2024-08-15 15:45:49 -0400

    working on admin borrow book class

--- app/src/main/java/com/example/project_firstpage/Admin_borrow.java
+++ app/src/main/java/com/example/project_firstpage/Admin_borrow.java
@@ -1,9 +1,12 @@
 package com.example.project_firstpage;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Admin_borrow extends AppCompatActivity {
@@ -22,15 +30,48 @@ public class Admin_borrow extends AppCompatActivity {
     private TextView noUserMsg;
     private Button btn_back;
 
+    private DatabaseReference borroeBooksRef, usersRef;
+    FirebaseAuth mAuth;
+    private  String userId;
+    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_borrow);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Book borrowed users");
+        setSupportActionBar(toolbar);
+
+        noUserMsg = findViewById(R.id.noUserMsg);
+        btn_back = findViewById(R.id.btn_back);
+
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
+
+        userListView = findViewById(R.id.userListView);
+
+
+
+
+        users = new ArrayList<>();
+        adapter = new AdminborrowbookAdapter(this, users);
+        userListView.setAdapter(adapter);
+
+        borroeBooksRef = FirebaseDatabase.getInstance().getReference("borrow");
+
+        loadBorrowUsers();
+
     }
+
+
 }
+
